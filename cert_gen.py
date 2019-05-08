fd92b9580d2f225cb179310f49877e06b6cea823
Kyle Robertson <kyle.wesley@me.com>
2019-05-08 16:42:34 -0400

    Began adding selenium code for Microsoft CA interaction

--- cert_gen.py
+++ cert_gen.py
@@ -6,45 +6,96 @@ import logging as log
 import inquirer
 import copy
 import glob
-import certsrv
-import warnings
-import contextlib
-
-import requests
-from urllib3.exceptions import InsecureRequestWarning
 
 
-old_merge_environment_settings = requests.Session.merge_environment_settings
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 
-@contextlib.contextmanager
-def no_ssl_verification():
-    opened_adapters = set()
+class MicrosoftCA:
 
-    def merge_environment_settings(self, url, proxies, stream, verify, cert):
-        # Verification happens only once per connection so we need to close
-        # all the opened adapters once we're done. Otherwise, the effects of
-        # verify=False persist beyond the end of this context manager.
-        opened_adapters.add(self.get_adapter(url))
 
-        settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
-        settings['verify'] = False
+    def __init__(self, fqdn, user, passwd):
+        opts = Options()
+        # opts.set_headless()
+        # assert opts.headless
+        self.browser = Chrome(options=opts)
+        self.wait = WebDriverWait(self.browser, 100)
+        self.URL = "https://{}:{}@{}/certsrv".format(user, passwd, fqdn)
+        self.browser.get(self.URL)
 
-        return settings
+    def open_time_entry_sheet(self):
+        """
+        Open the time entry sheet for the first time, starting from the homepage
+        """
+        # TODO: Validate we are at the homepage before attempting this
+        self.wait.until(EC.element_to_be_clickable((By.ID, "openTimeEntry")))
+        button = self.browser.find_element_by_id("openTimeEntry")
+        result = button.click()
+        print(result)
 
-    requests.Session.merge_environment_settings = merge_environment_settings
+    def _check_so_list(self, so_num):
+        """
+        Check the SO drop down on the time entry page for the given SO number
+        """
+        print("so_num = {}".format(so_num))
+        # self.wait.until(EC.element_to_be_clickable((By.NAME, "select-so")))
+        # sel_el = self.browser.find_element_by_name("select-so")
+        # print(sel_el)
+        # print("sel_el.text = {}".format(sel_el.text))
+        # actions = ActionChains(self.browser)
+        # actions.pause(2)
+        # actions.move_to_element_with_offset(sel_el, 2, 2)
+        # actions.pause(2)
+        # actions.click(sel_el)
+        # result = actions.perform()
+        # select_obj = Select(sel_el)
+        # print(select_obj.options)
 
-    try:
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', InsecureRequestWarning)
-            yield
-    finally:
-        requests.Session.merge_environment_settings = old_merge_environment_settings
+        div = self.browser.find_element_by_name("cont_with_so")
+        print("div = {}".format(div))
 
-        for adapter in opened_adapters:
-            try:
-                adapter.close()
-            except:
-                pass
+
+        # element = self.browser.find_element_by_xpath("//select[@name='select-so']")
+        # print("element = {}".format(element))
+        # all_options = element.find_elements_by_tag_name("option")
+        # print("all_options = {}".format(all_options))
+        # for option in all_options:
+        #     print("Value is: %s" % option.get_attribute("value"))
+            # option.click()
+        # el.click()
+        # options = el.find_elements_by_tag_name("option")
+        # print(options)
+
+        import pdb
+        pdb.set_trace()  # XXX BREAKPOINT
+
+    def _toggle_form(self):
+        # self.wait.until(EC.element_to_be_clickable((By.ID, "control_connect2")))
+        # el = self.browser.find_element_by_id("control_connect2")
+        el = self.browser.find_element_by_css_selector(
+            "input#control_connect2.widget-switch.checkbox-sw.checkbox"
+        )
+        print(el)
+        print(el.get_attribute("name"))
+        actions = ActionChains(self.browser)
+        actions.move_to_element(el)
+        actions.click(el)
+        result = actions.perform()
+        print("result = {}".format(result))
+
+    def create_entry(self, event):
+        # First check if the SO is in the SO dropdown list. This determines whether or
+        # not we continue with the default form or use a "custom" form
+        # self._toggle_form()
+        if self._check_so_list(event.so_number):
+            print("Use default form!")
+        else:
+            print("Use custom form!")
 
 # log.basicConfig(filename='cert_gen.log',level=log.DEBUG)
 
@@ -154,7 +205,7 @@ def convert_ca_cert_chain_to_base64_pem(infile, outfile=''):
     return outfile
 
 
-def submit_all_csrs_in_dir(csr_dir):
+def submit_all_csrs_to_microsoft_ca(csr_dir):
     """
     Looks for all CSR files under `csr_dir` recursively and submits a Certificate
     Signing Request to a Microsoft CA
@@ -162,15 +213,22 @@ def submit_all_csrs_in_dir(csr_dir):
 
     # This submits the CSR to the CA for the environment. The CA signing the
     # private keys created above so people know we are who we say we are
+    # questions = [
+    #   inquirer.Text('fqdn', message="CA Server FQDN"),
+    #   inquirer.Text('username', message="Domain Admin Username"),
+    #   inquirer.Text('password', message="Domain Admin Password"),
+    # ]
+    # srv_info = inquirer.prompt(questions)
     srv_info = {"fqdn": "ca2.vcloud.wei", "username": "kr", "password": "Worldcom1"}
-    ca_srvr = certsrv.Certsrv(srv_info["fqdn"], srv_info["username"],
-            srv_info["password"])
+    # ca_srvr = certsrv.Certsrv(srv_info["fqdn"], srv_info["username"],
+    #         srv_info["password"])
+    ca_srvr = MicrosoftCA(srv_info['fqdn'], srv_info["username"], srv_info["password"])
+    input("Continue?")
     csr_files = glob.glob(os.path.join(csr_dir, '**', '*.csr'))
     print("csr_files = {}".format(csr_files))
-    with no_ssl_verification():
-        for csr in csr_files:
-            cert = ca_srvr.get_cert(csr, "Administrator")
-            print(cert)
+    # for csr in csr_files:
+    #     cert = ca_srvr.get_cert(csr, "Administrator")
+    #     print(cert)
 
 def generate_self_signed_certs(csr_dir):
     """
@@ -199,15 +257,20 @@ def main():
     answers['solns'] = [el.replace(" ", "_") for el in answers['solns']]
 
     print("Enter configurations common to all certs below:")
-    questions = [
-      inquirer.Text('countryName', message="countryName", validate=validate_countryname),
-      inquirer.Text('stateOrProvinceName', message="stateOrProvinceName"),
-      inquirer.Text('localityName', message="localityName"),
-      inquirer.Text('organizationName', message="organizationName"),
-      inquirer.Text('organizationalUnitName', message="organizationalUnitName"),
-      # inquirer.Text('commonName', message="commonName"),
-    ]
-    answers["sslconfig"] = inquirer.prompt(questions)
+    # questions = [
+    #   inquirer.Text('countryName', message="countryName", validate=validate_countryname),
+    #   inquirer.Text('stateOrProvinceName', message="stateOrProvinceName"),
+    #   inquirer.Text('localityName', message="localityName"),
+    #   inquirer.Text('organizationName', message="organizationName"),
+    #   inquirer.Text('organizationalUnitName', message="organizationalUnitName"),
+    #   # inquirer.Text('commonName', message="commonName"),
+    # ]
+    # answers["sslconfig"] = inquirer.prompt(questions)
+    answers["sslconfig"] = {"countryName": "US",
+                            "stateOrProvinceName": "NH",
+                            "localityName": "Salem",
+                            "organizationName": "WEI",
+                            "organizationalUnitName": "Eng"}
     print("answers = {}".format(answers))
     # This generates all the Certificate Signing Requests (.csr files) and the private
     # keys.
@@ -242,7 +305,7 @@ def main():
 
     answers = inquirer.prompt(questions)
     func_lookup = {"Self Signed": generate_self_signed_certs,
-                   "Microsoft CA": submit_all_csrs_in_dir}
+                   "Microsoft CA": submit_all_csrs_to_microsoft_ca}
     func_lookup[answers['ca']](os.path.abspath("certs"))
     # submit_all_csrs_in_dir(os.path.abspath("certs"))
     
