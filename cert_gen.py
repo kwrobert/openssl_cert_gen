01345b9efacf9b881ee7a2d2476d11ffdea54b6d
Kyle Robertson <kyle.wesley@me.com>
2020-04-23 10:21:36 -0400

    Bug Fixes with Cert Generation
    Fixed bug where common name was not added to list of subject alternate
    names. This is now done automatically and fixed.
    
    Fixed a bug with cert chain conversion file type lookup.
    
    Added dependencies to VC
    
    Ran black to clean up some formatting

--- cert_gen.py
+++ cert_gen.py
@@ -6,6 +6,8 @@ import logging as log
 import inquirer
 import copy
 import glob
+import time
+import shutil
 
 
 from selenium.webdriver import Chrome
@@ -15,6 +17,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
+
 
 class MicrosoftCA:
     def __init__(self, fqdn, user, passwd, download_dir=None):
@@ -59,10 +62,10 @@ class MicrosoftCA:
         self.browser.find_element_by_link_text("Download certificate").click()
         self.browser.find_element_by_link_text("Download certificate chain").click()
 
+
 # log.basicConfig(filename='cert_gen.log',level=log.DEBUG)
 
-conf_template = \
-"""
+conf_template = """
 [ req ]
 default_bits = 2048
 default_keyfile = solution_name.key
@@ -85,6 +88,7 @@ organizationalUnitName = {organizationalUnitName}
 commonName = {commonName}
 """
 
+
 def validate_subjaltname(answers, current):
     valid = True
     for s in current.split(","):
@@ -92,7 +96,8 @@ def validate_subjaltname(answers, current):
             print("Name {} invalid. Must begin with 'DNS:' or 'IP:'".format(s))
             valid = False
             break
-    return valid 
+    return valid
+
 
 def validate_countryname(answers, current):
     valid = True
@@ -102,30 +107,37 @@ def validate_countryname(answers, current):
     return valid
 
 
-def generate_new_cert(outdir, name, common_conf):
+def generate_new_csr_and_key(outdir, name, common_conf):
     """
     Generate a new certificate configuration and certificate in `outdir` using `name`
     for the name of all files without the extension
     """
 
-    print("Generating cert {}".format(name))
+    print("Generating csr and key {}".format(name))
     conf = copy.copy(common_conf)
     questions = [
-      inquirer.Text('commonName', message="commonName"),
-      inquirer.Text('subjectAltName', message="subjectAltName (can be a csv)",
-          validate=validate_subjaltname),
+        inquirer.Text("commonName", message="commonName"),
+        inquirer.Text(
+            "subjectAltName",
+            message="subjectAltName (can be a csv)",
+            validate=validate_subjaltname,
+        ),
     ]
     conf.update(inquirer.prompt(questions))
+    # Must add the common name to the list of subject alternate names
+    conf["subjectAltName"] = "DNS:" + conf["commonName"] + "," + conf["subjectAltName"]
     print("conf = {}".format(conf))
     # Write the config file
-    conf_path = os.path.join(outdir, name+".cfg")
-    key_path = os.path.join(outdir, name+".key.encrypted")
-    csr_path = os.path.join(outdir, name+".csr")
-    with open(conf_path, 'w') as f:
+    conf_path = os.path.join(outdir, name + ".cfg")
+    key_path = os.path.join(outdir, name + ".key.encrypted")
+    csr_path = os.path.join(outdir, name + ".csr")
+    with open(conf_path, "w") as f:
         f.write(conf_template.format(**conf))
     # Make the keys
-    cmd = ("openssl req -new -nodes -out {csr_path} -keyout {key_path} -config "
-          "{conf_path}")
+    cmd = (
+        "openssl req -new -nodes -out {csr_path} -keyout {key_path} -config "
+        "{conf_path}"
+    )
     cmd = cmd.format(key_path=key_path, csr_path=csr_path, conf_path=conf_path)
     print("cmd = {}".format(cmd))
     try:
@@ -139,26 +151,26 @@ def generate_new_cert(outdir, name, common_conf):
     print("output = {}".format(output))
     # Decrypt the private key
     cmd = "openssl rsa -in {key_path} -out {out_path}"
-    cmd = cmd.format(key_path=key_path, out_path=key_path[0:key_path.rfind(".")])
+    cmd = cmd.format(key_path=key_path, out_path=key_path[0 : key_path.rfind(".")])
     subprocess.check_output(cmd, shell=True)
 
 
-def convert_ca_cert_chain_to_base64_pem(infile, outfile=''):
+def convert_ca_cert_chain_to_base64_pem(infile, outfile=""):
     """
     Converts the CA Certificate Chain contained in `infile` to the standard Base 64
     encoded X.509 format
     """
 
-    infile_path, infile_ext =  os.path.splitext(infile)
+    infile_path, infile_ext = os.path.splitext(infile)
     if not outfile:
-        outfile = infile_path + ".cer"
+        outfile = infile_path + "_pem_cert_chain.cer"
     # Maps infile extensions to the command required to convert them to PEM format. This
     # isn't fool proof because users can put any extension they want on a file, but that
     # doesn't mean it's actually in that format
     # TODO: Find a way to identify format of infile from its contents
-    cmds = {"p7b": "openssl pkcs7 -print_certs -in {infile} -out {outfile}"} 
+    cmds = {"p7b": "openssl pkcs7 -print_certs -in {infile} -out {outfile}"}
     try:
-        cmd = cmds[infile_ext]
+        cmd = cmds[infile_ext.strip(".")]
     except KeyError as e:
         e.args = ("Cannot convert CA Cert chain from {} format".format(infile_ext),)
         raise
@@ -190,12 +202,11 @@ def submit_all_csrs_to_microsoft_ca(csr_dir):
     #         srv_info["password"])
     csr_files = glob.glob(os.path.join(csr_dir, "**", "*.csr"))
     print("csr_files = {}".format(csr_files))
-    download_dir = os.path.abspath(os.path.split(csr_files[0])[0])
     ca_srvr = MicrosoftCA(
         srv_info["fqdn"],
         srv_info["username"],
         srv_info["password"],
-        download_dir=download_dir,
+        download_dir=csr_dir,
     )
     # for csr in csr_files:
     #     cert = ca_srvr.get_cert(csr, "Administrator")
@@ -210,51 +221,73 @@ def submit_all_csrs_to_microsoft_ca(csr_dir):
         # wait to finish downloading before moving
         time.sleep(2)
         # Rename downloaded certs
-        csr_file_name_with_ext = os.path.split(csr_file)[-1]
+        existing_certs = glob.glob(os.path.join(csr_dir, "**", "*certnew*"))
+        for old_cert in existing_certs:
+            print(f"Removing existing cert {old_cert}")
+            os.remove(old_cert)
+        csr_file_path, csr_file_name_with_ext = os.path.split(csr_file)
         csr_file_name = os.path.splitext(csr_file_name_with_ext)[0]
-        signed_cert_download_path = os.path.join(download_dir, "certnew.cer")
-        cert_chain_download_path = os.path.join(download_dir, "certnew.p7b")
-        signed_cert_new_name = os.path.join(download_dir, f"{csr_file_name}_signed_cert.cer")
-        cert_chain_new_name = os.path.join(download_dir, f"{csr_file_name}_cert_chain.p7b")
+        signed_cert_download_path = os.path.join(csr_dir, "certnew.cer")
+        cert_chain_download_path = os.path.join(csr_dir, "certnew.p7b")
+        signed_cert_new_name = os.path.join(
+            csr_file_path, f"{csr_file_name}_signed_cert.cer"
+        )
+        cert_chain_new_name = os.path.join(
+            csr_file_path, f"{csr_file_name}_cert_chain.p7b"
+        )
         shutil.move(signed_cert_download_path, signed_cert_new_name)
         shutil.move(cert_chain_download_path, cert_chain_new_name)
+        # Finally, convert the PKCS #7 cert chain provided by Microsoft CAs to PEM
+        # format
+        convert_ca_cert_chain_to_base64_pem(cert_chain_new_name)
     ca_srvr.browser.quit()
+    return 
+
 
 def generate_self_signed_certs(csr_dir):
     """
     """
-    csr_files = glob.glob(os.path.join(csr_dir, '**', '*.csr'))
+    csr_files = glob.glob(os.path.join(csr_dir, "**", "*.csr"))
     print("csr_files = {}".format(csr_files))
     for csr in csr_files:
         name, ext = os.path.splitext(csr)
         certfile = name + ".cert"
         keyfile = name + ".key"
-        cmd = "openssl x509 -in {} -out {} -req -signkey {} -days 1001".format(csr, certfile, keyfile)
+        cmd = "openssl x509 -in {} -out {} -req -signkey {} -days 1001".format(
+            csr, certfile, keyfile
+        )
         subprocess.check_output(cmd, shell=True)
+
 
 def main():
 
+    map_soln_to_csr_names = {
+        "vRealize_Automation_Medium_Distributed": ["IaaS-Web", "IaaS-Manager", "Appliances"],
+        "vRealize_Automation_Small": ["IaaS", "Appliances"],
+        "vRealize_Business": ["vBusiness"],
+        "vRealize_Operations": ["vROPS"],
+    }
     questions = [
-      inquirer.Checkbox('solns',
-                        message="What are you interested in?",
-                        choices=['vRealize Automation',
-                                 'vRealize Automation Distributed',
-                                 'vRealize Business',
-                                 'vRealize Operations'],
-                        ),
+        inquirer.Checkbox(
+            "solns",
+            message="What solutions are you deploying (select multiple with spacebar)?",
+            choices=list(map_soln_to_csr_names.keys()),
+        )
     ]
     answers = inquirer.prompt(questions)
-    answers['solns'] = [el.replace(" ", "_") for el in answers['solns']]
+    answers["solns"] = [el.replace(" ", "_") for el in answers["solns"]]
 
     print("Enter configurations common to all certs below:")
-    # questions = [
-    #   inquirer.Text('countryName', message="countryName", validate=validate_countryname),
-    #   inquirer.Text('stateOrProvinceName', message="stateOrProvinceName"),
-    #   inquirer.Text('localityName', message="localityName"),
-    #   inquirer.Text('organizationName', message="organizationName"),
-    #   inquirer.Text('organizationalUnitName', message="organizationalUnitName"),
-    #   # inquirer.Text('commonName', message="commonName"),
-    # ]
+    questions = [
+        inquirer.Text(
+            "countryName", message="countryName", validate=validate_countryname
+        ),
+        inquirer.Text("stateOrProvinceName", message="stateOrProvinceName"),
+        inquirer.Text("localityName", message="localityName"),
+        inquirer.Text("organizationName", message="organizationName"),
+        inquirer.Text("organizationalUnitName", message="organizationalUnitName"),
+        # inquirer.Text('commonName', message="commonName"),
+    ]
     # answers["sslconfig"] = inquirer.prompt(questions)
     answers["sslconfig"] = {"countryName": "US",
                             "stateOrProvinceName": "NH",
@@ -264,20 +297,15 @@ def main():
     print("answers = {}".format(answers))
     # This generates all the Certificate Signing Requests (.csr files) and the private
     # keys.
-    for soln in answers['solns']:
+    for soln in answers["solns"]:
         out = os.path.join("certs", soln)
         print("out = {}".format(out))
         if not os.path.isdir(out):
             os.makedirs(out)
-        if "vRealize_Automation" in soln:
-            print("Generating vRealize Automation Certs ...")
-            distrib = False
-            if "Distributed" in soln:
-                names = ("IaaS-Web", "IaaS-Manager", "VCAC")
-            else:
-                names = ("IaaS", "VCAC")
-            for name in names:
-                generate_new_cert(out, name, answers["sslconfig"])
+        print(f"Generating certificate signing requests for {soln}")
+        names = map_soln_to_csr_names[soln]
+        for name in names:
+            generate_new_csr_and_key(out, name, answers["sslconfig"])
 
     # questions = [
     #   inquirer.Text('fqdn', message="CA Server FQDN"),
@@ -286,19 +314,23 @@ def main():
     # ]
     # srv_info = inquirer.prompt(questions)
 
+    print("BEGINNING SIGNING PROCESS ...")
     questions = [
-        inquirer.List('ca',
-                      message="What kind of certificate authority are you using?",
-                      choices=['Self Signed', 'Microsoft CA'],
-                  ),
+        inquirer.List(
+            "ca",
+            message="What kind of certificate authority are you using?",
+            choices=["Self Signed", "Microsoft CA"],
+        )
     ]
 
     answers = inquirer.prompt(questions)
-    func_lookup = {"Self Signed": generate_self_signed_certs,
-                   "Microsoft CA": submit_all_csrs_to_microsoft_ca}
-    func_lookup[answers['ca']](os.path.abspath("certs"))
+    func_lookup = {
+        "Self Signed": generate_self_signed_certs,
+        "Microsoft CA": submit_all_csrs_to_microsoft_ca,
+    }
+    func_lookup[answers["ca"]](os.path.abspath("certs"))
     # submit_all_csrs_in_dir(os.path.abspath("certs"))
-    
+
 
 if __name__ == "__main__":
     main()
